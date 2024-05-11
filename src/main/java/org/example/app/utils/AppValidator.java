1fc4705fa97f9a124d3cd012df09f5e2c41543b5
vnovikov <nik.hbb@gmail.com>
2024-05-11 05:38:57 +0300

    Add utils

---
+++ src/main/java/org/example/app/utils/AppValidator.java
@@ -0,0 +1,28 @@
+package org.example.app.utils;
+
+public class AppValidator {
+
+    // Regex для id.
+    // Регулярний вираз для позитивних цілих чисел без нулів, мінусів або ком на початку.
+    // ^ вказує на початок рядка
+    // [1-9] відповідає будь-якій цифрі від 1 до 9 і гарантує відсутність нулів на початку.
+    // $ вказує на кінець рядка
+    public final static String ID_RGX = "^[1-9]$";
+
+    // Regex для телефону у форматі xxx xxx-xxxx
+    public final static String EMAIL_RGX = "^((?!\\.)[\\w\\-_.]*[^.])(@\\w+)(\\.\\w+(\\.\\w+)?[^.\\W])$";
+
+    // Валідація вхідного значення id
+    public static boolean isIdValid(String id) {
+        if (id != null)
+            return id.isEmpty() || !id.matches(ID_RGX);
+        return false;
+    }
+
+    // Валідація вхідного значення телефона
+    public static boolean isEmailValid(String email) {
+        if (email != null)
+            return email.isEmpty() || !email.matches(EMAIL_RGX);
+        return false;
+    }
+}
